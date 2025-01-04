df549b45c4a12ee9946c8f4b53d33827fdeeb708
mdzahidhasan53 <mzhasan53@gmail.com>
2025-01-04 21:47:04 +0600

    calculate avg rating, total movie number

--- src/index.js
+++ src/index.js
@@ -1,2 +1,43 @@
 import { getMovieReviewData } from "./data.js";
 
+function init () {
+    const movieReviewData = getMovieReviewData();
+
+    paintStatistics (movieReviewData);
+
+}
+
+function paintStatistics (movieReviewData) {
+    const flatReviewData = movieReviewData.flat();
+
+    const totalMovies = movieReviewData.length;
+    const totalReviews = flatReviewData.length;
+    const totalRating = flatReviewData.reduce((acc, item) => {
+        return acc + item.rating;
+    }, 0);
+
+    console.log(totalRating);
+
+    const avgRating = (totalRating / totalReviews).toFixed(2);
+    console.log(avgRating);
+    const totalMoviesEl = document.getElementById("tMovie");
+    addStat (totalMoviesEl, totalMovies);
+
+    const totalRatingEl = document.getElementById("tAvgRating");
+    addStat (totalRatingEl, avgRating);
+
+    const totalReviewEl = document.getElementById("tReview");
+    addStat (totalReviewEl, totalReviews);
+
+
+
+}
+
+function addStat (elem, value) {
+    const spanEl = document.createElement("span");
+    spanEl.classList.add("text-3xl")
+    spanEl.innerText = value;
+    elem.appendChild(spanEl);
+}
+
+init();
